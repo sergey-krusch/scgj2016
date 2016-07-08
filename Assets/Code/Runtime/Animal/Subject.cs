af394645de502b5f8062c67552a6cd7c119a2e5a
Sergey <krooshch@gmail.com>
2016-07-08 22:47:36 +1000

    Working on animal and digger code

---
+++ Assets/Code/Runtime/Animal/Subject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Subject: MonoBehaviour
+{
+    public GameObject EmptyState;
+    public GameObject NormalState;
+    public GameObject FullState;
+
+    public float EmptyNormalBound;
+    public float NormalFullBound;
+
+    public float Value;
+
+    public void Update()
+    {
+        if (Value < EmptyNormalBound)
+            SwitchTo(EmptyState);
+        else if (Value < NormalFullBound)
+            SwitchTo(NormalState);
+        else
+            SwitchTo(FullState);
+    }
+
+    private void SwitchTo(GameObject go)
+    {
+        EmptyState.SetActive(false);
+        NormalState.SetActive(false);
+        FullState.SetActive(false);
+        go.SetActive(true);
+    }
+}
