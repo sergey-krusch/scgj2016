be6ef5e32dbda70ef8fe2031e09eb58bc3fc6d11
Sergey <krooshch@gmail.com>
2016-07-09 00:24:02 +1000

    ActionInvoker initial commit

---
+++ Assets/Code/Runtime/ActionInvoker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ActionInvoker
+{
+    public static void Invoke(Action action)
+    {
+        if (action != null)
+            action();
+    }
+
+    public static void Invoke<T>(Action<T> action, T arg1)
+    {
+        if (action != null)
+            action(arg1);
+    }
+}
