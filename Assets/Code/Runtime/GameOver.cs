88a585f80038596da8ef7819da4f4e628439c32d
Sergey <krooshch@gmail.com>
2016-07-09 16:48:03 +1000

    GameOver takes a pause before reacting to tapping

--- Assets/Code/Runtime/GameOver.cs
+++ Assets/Code/Runtime/GameOver.cs
@@ -6,16 +6,25 @@ public class GameOver: MonoBehaviour
 {
     public static string Reason;
 
+    public float Pause;
     public Text ScoreLabel;
+    public Text TapAnywhereLabel;
+    private float remainingTime;
 
     public void Awake()
     {
+        remainingTime = Pause;
         ScoreLabel.text = string.Format(ScoreLabel.text, Session.Score);
     }
 
     public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+        remainingTime -= Time.deltaTime;
+        if (remainingTime < 0)
+        {
+            TapAnywhereLabel.gameObject.SetActive(true);
+            if (Input.GetMouseButtonDown(0))
+                SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+        }
     }
 }
