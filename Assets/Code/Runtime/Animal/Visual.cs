f7056b9ebff6d2eb4deb61b3a71a8a92b688a777
David Page <David Page>
2016-07-10 11:45:42 +1000

    Converted to 3D

--- Assets/Code/Runtime/Animal/Visual.cs
+++ Assets/Code/Runtime/Animal/Visual.cs
@@ -5,50 +5,43 @@ namespace Animal
 {
     public class Visual: MonoBehaviour
     {
-        public GameObject EmptyState;
-        public GameObject NormalState;
-        public GameObject FullState;
+		public GameObject AliveState;
         public GameObject DeadState;
 
         public bool Dead;
         public float Value;
+		public SkinnedMeshRenderer MeshRenderer;
 
         public void Awake()
         {
             var cfg = Root.Instance.Animal;
             var i = Random.Range(0, cfg.Colors.Length);
             var c = cfg.Colors[i];
-            SetColor(EmptyState, c);
-            SetColor(NormalState, c);
-            SetColor(FullState, c);
+            SetColor(AliveState, c);
             SetColor(DeadState, c);
         }
 
         public void Update()
         {
             var cfg = Root.Instance.Animal;
-            if (Dead)
-                SwitchTo(DeadState);
-            else if (Value < cfg.EmptyNormalBound)
-                SwitchTo(EmptyState);
-            else if (Value < cfg.NormalFullBound)
-                SwitchTo(NormalState);
-            else
-                SwitchTo(FullState);
+			if (Dead)
+				SwitchTo (DeadState);
+			else
+			{
+				SwitchTo (AliveState);
+				MeshRenderer.SetBlendShapeWeight (1, (1f - Value) * 100f);
+			}
         }
 
         private void SwitchTo(GameObject go)
         {
-            EmptyState.SetActive(false);
-            NormalState.SetActive(false);
-            FullState.SetActive(false);
+            AliveState.SetActive(false);
             DeadState.SetActive(false);
             go.SetActive(true);
         }
 
         private void SetColor(GameObject o, Color c)
         {
-            o.GetComponent<SpriteRenderer>().color = c;
         }
     }
 }
